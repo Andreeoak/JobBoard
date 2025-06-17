5807aae1efebd5e229c69856658e273d7f17bbf1
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-17 18:43:58 -0300

    Styling the index and making card component

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     @foreach ( $jobs as $job)
-        <div>{{$job->title}}</div>
+        <div class = "rounded-md border-slate-300 bg-white p-4 shadow-sm mb-4">{{$job->title}}</div>
     @endforeach
 </x-layout>
