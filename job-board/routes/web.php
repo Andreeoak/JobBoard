5cc847887e7f063f80829023eba012b5349bc22c
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-19 12:47:39 -0300

    Adding tag compenent and route to jobs

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource("jobs", JobController::class)->only(["index"]);
+
+Route::get("", fn()=>to_route("jobs.index"));
