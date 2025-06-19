e41f4bbb318d95f0a5051fb7ed3328cfbba6890e
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-19 18:09:56 -0300

    JobCard and buttons for Details

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource("jobs", JobController::class)->only(["index"]);
+Route::resource("jobs", JobController::class)->only(["index", "show"]);
 
 Route::get("", fn()=>to_route("jobs.index"));
