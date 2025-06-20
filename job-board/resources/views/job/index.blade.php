ec33321949d973241cb676d951585128614326a2
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-20 19:44:27 -0300

    Fixing salary form search

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -15,7 +15,7 @@
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex">
                         <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max-salary" value="{{ request('max') }}" placeholder="Search for any text" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="Search for any text" />
                     </div>
                 </div>
             </div>
