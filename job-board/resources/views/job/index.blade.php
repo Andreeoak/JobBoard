8bba17d9562fc2592eb5b4e74efffed1556d5b49
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-21 16:13:06 -0300

    Filtering jobs using Javascript

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -5,17 +5,17 @@
     />
 
     <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text"/>
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" form-Id="filtering-form"/>
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="Search for any text" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" form-Id="filtering-form"/>
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="Search for any text" form-Id="filtering-form"/>
                     </div>
                 </div>
                 <div>
