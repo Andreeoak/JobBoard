f3cfbe58a1f0b2133a11111f26089baa3cb85e2b
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-22 14:38:27 -0300

    Improving the show blade for jobs of the same employer

--- job-board/resources/views/job/show.blade.php
+++ job-board/resources/views/job/show.blade.php
@@ -10,4 +10,29 @@
         </p>
     </x-job-card>
 
+    <x-card class="mb-4">
+        <h2 class="mb-4 text-lg font-medium">More {{ optional($job->employer)->company_name }} Jobs</h2>
+
+        <div class="text-sm text-slate-500">
+            @foreach ($job->employer->jobs as $otherjob)
+                <div class="mb-4 flex justify-between">
+                    <div>
+                        <div class="text-xs text-slate-700">
+                            <a href="{{ route('jobs.show', $otherjob) }}">
+                                {{ $otherjob->title }}
+                            </a>
+                        </div>
+                        <div class="text-xs">
+                            {{ $otherjob->created_at->diffForHumans() }}
+                        </div>
+                    </div>
+                    <div class="text-xs self-center">
+                        ${{ number_format($otherjob->salary) }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </x-card>
+
 </x-layout>
