2b79d3e5818b107bf4a79b62a9f61e1e91e8adc5
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-26 20:55:10 -0300

    Creating JobApplications

--- job-board/database/seeders/DatabaseSeeder.php
+++ job-board/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Employer;
 use \App\Models\Job;
+use App\Models\JobApplication;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -37,6 +38,19 @@ class DatabaseSeeder extends Seeder
                     "employer_id"=> $employers->random()->id
                 ]
             );
+        }
+
+        foreach($users as $user)
+        {
+            $jobs = Job::inRandomOrder()->take(rand(0,4))->get();
+
+            foreach($jobs as $job)
+            {
+                JobApplication::factory()->create([
+                    'job_id'=> $job->id,
+                    'user_id'=> $user->id
+                ]);
+            }
         }
     }
 }
