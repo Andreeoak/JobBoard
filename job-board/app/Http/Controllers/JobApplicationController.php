76442e8d7ee7237f794bf6d386277cd9613323ef
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-28 20:24:47 -0300

    Feature CV upload

--- job-board/app/Http/Controllers/JobApplicationController.php
+++ job-board/app/Http/Controllers/JobApplicationController.php
@@ -23,9 +23,18 @@ class JobApplicationController extends Controller
      */
     public function store(Job $job, Request $request)
     {
+        $this->authorize('apply', $job);
+
+        $validatedData = $request->validate([
+            'expected_salary'=>'required|min:1|max:10000',
+            "cv" => 'required|file|mimes:pdf|max:2500'
+        ]);
+
+
         $job->jobApplications()->create([
             'user_id'=> $request->user()->id,
-            ...$request->validate(['expected_salary'=>'required|min:1|max:10000'])
+            'expected_salary' => $validatedData['expected_salary'],
+            'cv_path' => $request->file('cv')->store('cvs', 'private')
         ]);
 
         return redirect()->route("jobs.show", $job)->with('success', 'You have successfully applied!');
--- job-board/resources/views/job_application/create.blade.php
+++ job-board/resources/views/job_application/create.blade.php
@@ -10,7 +10,8 @@
         <h2 class="mb-4 text-lg font-medium">
             Your job application
         </h2>
-        <form action="{{ route('job.application.store', $job) }}" method="POST">
+        <form action="{{ route('job.application.store', $job) }}" method="POST"
+        enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
                 <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">
@@ -18,7 +19,12 @@
                 </label>
                 <x-text-input type="number" name="expected_salary"/>
             </div>
-
+            <div>
+                <label class="mb-2 black text-sm font-medium text-slate-900">
+                    Upload CV
+                </label>
+                <x-text-input type="file" name="cv"/>
+            </div>
             <x-button class="w-full"> Apply</x-button>
         </form>
     </x-card>
