422cc116cf9be932333756a87795e3309e36e77d
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-28 11:14:14 -0300

    Extra information on job card reggarding your application and others

--- job-board/app/Http/Controllers/MyJobApplicationController.php
+++ job-board/app/Http/Controllers/MyJobApplicationController.php
@@ -8,7 +8,17 @@ class MyJobApplicationController extends Controller
 {
     public function index()
     {
-
+        return view("my_job_applications.index",
+        [
+                'applications'=>\Illuminate\Support\Facades\Auth::user()->jobApplications()
+                ->with([
+                    'job' => function($q) {
+                        $q->withCount('jobApplications')->withAvg('jobApplications', 'expected_salary');
+                    },
+                    'job.employer'
+                ])->latest()->get()
+        ]
+        );
     }
 
     public function destroy()
